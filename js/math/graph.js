b80411d66b54fd58a263a2c05f2536c8972076a6
Hem Chandra Upadhyay <hemupadhyay444@gmail.com>
2024-04-20 16:22:05 +0530

    add: function component to the graph class and the segment and point class also start mouse input functionality

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,61 @@ class Graph {
     this.segments = segments;
   }
 
+  addPoint(point) {
+    this.points.push(point);
+  }
+  addSegement(seg) {
+    this.segments.push(seg);
+  }
+
+  containsPoint(point) {
+    return this.points.find((p) => p.equals(point));
+  }
+  containsSegment(seg) {
+    return this.segments.find((s) => s.equals(seg));
+  }
+
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
+  tryAddSegment(seg) {
+    if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+      this.addSegement(seg);
+      return true;
+    }
+    return false;
+  }
+
+  removeSegment(seg) {
+    this.segments.splice(this.segments.indexOf(seg), 1);
+  }
+  removePoint(point) {
+    const segs = this.getSegmentsWithPoint(point);
+    for (const seg of segs) {
+      this.removeSegment(seg);
+    }
+    this.points.splice(this.points.indexOf(point), 1);
+  }
+
+  getSegmentsWithPoint(point) {
+    const segs = [];
+    for (const seg of this.segments) {
+      if (seg.includes(point)) {
+        segs.push(seg);
+      }
+    }
+    return segs;
+  }
+
+  dispose() {
+    this.points.length = 0;
+    this.segments.length = 0;
+  }
+
   draw(ctx) {
     for (const seg of this.segments) {
       seg.draw(ctx);
